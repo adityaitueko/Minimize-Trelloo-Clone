fefe518758214a2964af7c7f89c8ff8934b3615b
Asep Jumadi <43208840+asepjumadi@users.noreply.github.com>
2026-03-22 09:19:22 +0700

    modified and remove literral html to prevent duplicate html litteral using AuthSessionProvider components for support next js auth

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,12 +1,11 @@
 import "../globals.css";
 import { AuthSessionProvider } from "@/components/auth/SessionProvider";
+import { getServerSession } from "next-auth";
+import { authOptions } from "@/app/api/auth/[...nextauth]/authOptions";
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+export default async function RootLayout({ children }: { children: React.ReactNode }) {
+  const session = await getServerSession(authOptions);
   return (
-    <html lang="en" suppressHydrationWarning>
-      <body>
-        <AuthSessionProvider>{children}</AuthSessionProvider>
-      </body>
-    </html>
+    <AuthSessionProvider session={session}>{children}</AuthSessionProvider>
   );
 }
